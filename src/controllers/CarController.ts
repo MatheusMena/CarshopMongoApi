ef96103a0a0d83eaf9fcf7469d24882a477e8b9b
MatheusMena <matheusmena1@hotmail.com>
2022-08-24 15:41:04 -0300

    feat: função update na camada

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -38,4 +38,12 @@ export default class CarController {
     const getCarByID = await this._service.readOne(req.params.id);
     return res.status(200).json(getCarByID);
   }
+
+  public async update(
+    req: Request, 
+    res: Response<ICar | null>,
+  ) {
+    const result = await this._service.update(req.params.id, req.body);
+    return res.status(200).json(result);
+  }
 }
