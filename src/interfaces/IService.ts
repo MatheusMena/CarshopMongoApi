d6400d1f3b4f5a2e7dacfaf1ee3b474b6d605464
MatheusMena <matheusmena1@hotmail.com>
2022-08-24 14:22:55 -0300

    feat: implementa rota get /cars/:id

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,6 +1,6 @@
 interface IService<T> {
   create(obj:T):Promise<T>,
-  //   readOne(_id:string):Promise<T>,
+  readOne(_id:string):Promise<T>,
   //   update(_id: string, body: T): Promise<T | null>,
   read(): Promise<T[]>,
 //   delete(_id: string): Promise<T | null>,
