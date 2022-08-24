81a849b71e5c9045e14f4f062288c910670f1dc0
MatheusMena <matheusmena1@hotmail.com>
2022-08-24 13:37:53 -0300

    feat:Implementa router(carsRouter) e errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
+import carsRouter from './routes/carRoute';
+import errorHandler from './middlewares/error';
 
 const app = express();
-
+app.use(express.json());
+app.use(carsRouter);
+app.use(errorHandler);
 export default app;
