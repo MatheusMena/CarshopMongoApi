a91f31aa9275049e8de37a338d906953dfa286b0
MatheusMena <matheusmena1@hotmail.com>
2022-08-25 19:43:54 -0300

    feat:Implementa crud Motorcycle

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import carsRouter from './routes/carRoute';
+import motoRouter from './routes/motorcycleRoute';
 import errorHandler from './middlewares/error';
 
 const app = express();
 app.use(express.json());
 app.use(carsRouter);
+app.use(motoRouter);
 app.use(errorHandler);
 export default app;
